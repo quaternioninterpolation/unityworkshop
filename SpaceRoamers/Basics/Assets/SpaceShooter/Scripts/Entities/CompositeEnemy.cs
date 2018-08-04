8f6d071f7f90c704781d404fc79b034ae0c7c04e
Josh van den Heever <atomicafrica@gmail.com>
2018-08-04 19:15:11 +1200

    Yesus amounts of assets

---
+++ SpaceRoamers/Basics/Assets/SpaceShooter/Scripts/Entities/CompositeEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompositeEnemy : StrafeEnemy 
+{
+    private CompositeEnemyPart[] parts;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        //Get all child parts
+        parts = GetComponentsInChildren<CompositeEnemyPart>();
+        foreach (var part in parts)
+        {
+            part.SetDestroyCallback(OnPartDestroyed);
+            part.SetParent(this);
+        }
+    }
+
+    protected override void Think()
+    {
+        base.Think();
+
+        if (target != null)
+        {
+            foreach (var part in parts)
+            {
+                if (part != null && part.gameObject != null && part.IsAlive())
+                {
+                    part.FireWeapon(target);
+                }
+            }
+        }
+    }
+
+    protected virtual void OnPartDestroyed()
+    {
+        //Count remaining
+        int remaining = 0;
+        foreach (var part in parts)
+        {
+            if (part.IsAlive())
+                ++remaining;
+        }
+
+        if (remaining == 0)
+        {
+            Kill();
+        }
+        else
+        {
+            //Update our parts
+            parts = GetComponentsInChildren<CompositeEnemyPart>();
+        }
+    }
+
+    public override void Damage(int damageAmount)
+    {
+        //base.Damage(damageAmount);
+        //Don't damage the main body
+    }
+}
---
+++ SpaceRoamers/Basics/Assets/SpaceShooter/Scripts/Entities/CompositeEnemyPart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CompositeEnemyPart : BaseEntity 
+{
+    public Weapon weapon;
+
+    public void SetParent(BaseEntity parent)
+    {
+        team = parent.team;
+    }
+
+    public void FireWeapon(BaseEntity target)
+    {
+        Vector2 direction = (target.transform.position - weapon.transform.position).normalized;
+        weapon?.Fire(this, direction, 0f);
+    }
+}
